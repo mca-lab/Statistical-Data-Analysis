bf8066749ab7d70fe85451ca9b4e4df2862dffe2
Komal-Mathpal <komalmathpal1010@gmail.com>
2025-12-17 17:27:56 +0530

    completed Exercise 3

--- DescStats/Komal_Statistical.R
+++ DescStats/Komal_Statistical.R
@@ -10,4 +10,26 @@ students$Grade <- ifelse(students$Marks >= 75, "A",
                          ifelse(students$Marks >= 50, "B", "C"))
 students
 
+#Exercise-2
+nrow(iris)
+ncol(iris)
+head(iris)
+mean(iris$Sepal.Length)
+median(iris$Sepal.Length)
+var(iris$Sepal.Length)
+sd(iris$Sepal.Length)
+table(iris$Species)
+hist(iris$Petal.Length)
+boxplot(iris$Sepal.Width)
+
+#Exercise 3
+prior <- 0.01          # Probability of disease (1%)
+true_pos <- 0.95       # Sensitivity: P(test + | disease)
+false_pos <- 0.05      # P(test + | no disease)
+posterior <- (true_pos * prior) /
+  ((true_pos * prior) + (false_pos * (1 - prior)))
+
+posterior
+
+
 
